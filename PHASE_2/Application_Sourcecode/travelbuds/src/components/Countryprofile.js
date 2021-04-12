d59ed492723e8ddd07d426c94c818a6c26f4e9d4
Allen <allenwu564@hotmail.com>
2021-04-13 00:07:58 +1000

    started country profile

--- PHASE_2/Application_Sourcecode/travelbuds/src/components/Countryprofile.js
+++ PHASE_2/Application_Sourcecode/travelbuds/src/components/Countryprofile.js
@@ -4,11 +4,15 @@ import Sydney from "../images/Sydney.jpg"
 
 function Countryprofile() {
     return (
-        <div>
-            <img src={Sydney} alt="sydney"></img>
-            <div>
-                
+        <div className="country-profile">
+            <div className="img-container">
+                <img src={Sydney} alt="sydney"></img>
+                <div className="destination-name">
+                    <h1>City Name</h1>
+                    <h2>Country Name</h2>
+                </div>
             </div>
+            
         </div>
     )
 }
