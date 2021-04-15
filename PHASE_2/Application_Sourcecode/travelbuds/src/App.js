2b1ffcf2682aa6a1a7f90735f75b7bf48280232d
Allen <allenwu564@hotmail.com>
2021-04-15 14:00:09 +1000

    fixed requirements

--- PHASE_2/Application_Sourcecode/travelbuds/src/App.js
+++ PHASE_2/Application_Sourcecode/travelbuds/src/App.js
@@ -8,21 +8,21 @@ import Login from "./pages/Login.js"
 import Main from "./pages/Main.js"
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Login></Login>
-                </Route>
-                <Route path="/main">
-                    <Main></Main>
-                </Route>
-            </Switch>
-        </Router>
-        
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Login></Login>
+                    </Route>
+                    <Route path="/main">
+                        <Main></Main>
+                    </Route>
+                </Switch>
+            </Router>
+            
+        </div>
+    );
 }
 
 export default App;
